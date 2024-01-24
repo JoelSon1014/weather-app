f18514a945056a30793fd8764556889bab8f480b
Your Name <joelyson@umich.edu>
2024-01-24 14:31:30 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let weather = {
+    apikey: "7de9aa4d806b685d23f55208b0d6f1ee",
+    
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apikey)
+            .then(response => response.json())
+            .then(data => {
+                // Handle the weather data here
+                this.display_weather(data);
+            })
+            .catch(error => {
+                console.error("Error fetching weather data:", error);
+            });
+    },
+
+    display_weather: function (data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".condition").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind-speed").innerText = "Wind Speed: " + speed + " m/h";
+    }
+};
