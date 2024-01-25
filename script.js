52a1a06bf3050e3b3ac211b07eab04fe548f2478
Your Name <joelyson@umich.edu>
2024-01-25 15:06:21 -0500

    Finish beginning features

--- script.js
+++ script.js
@@ -19,9 +19,24 @@ let weather = {
         const {speed} = data.wind;
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".temp").innerText = Math.round(temp) + "°F";
         document.querySelector(".condition").innerText = description;
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind-speed").innerText = "Wind Speed: " + speed + " m/h";
+        document.querySelector(".wind-speed").innerText = "Wind Speed: " + Math.round(speed) + " mph";
+        document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`
+    },
+
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
